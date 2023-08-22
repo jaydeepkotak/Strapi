f70d7822928e356cebf4da98696a7b8505674f38
jaydep <jaydeep@addact.net>
2023-08-22 14:49:33 +0530

    Commit

--- backend/src/api/resturant/routes/resturant.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * resturant router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::resturant.resturant');
--- backend/src/api/resturant/services/resturant.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * resturant service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::resturant.resturant');
